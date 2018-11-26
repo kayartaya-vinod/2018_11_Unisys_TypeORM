367bf84302118058821bfb00835fbf1ba8983236
kayartaya.vinod <kayartaya.vinod@gmail.com>
2018-11-26 21:00:09 +0530

    🇮🇳

--- typeorm-basics/src/programs/t01.ts
+++
@@ -1,9 +0,0 @@
-import { createConnection } from "typeorm";
-import { Product } from "../entity/northwind/Product";
-
-(async ()=>{
-    const conn = await createConnection();
-    const p1 = await conn.manager.findOne(Product, 1);
-    console.log(p1);
-    await conn.close();
-})();
